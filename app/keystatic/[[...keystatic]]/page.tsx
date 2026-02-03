804cc0189749896f15a014a426b747ebf7baa5aa
Julio <adm.brandify@gmail.com>
2026-02-03 14:30:47 -0300

    fix: remove edge runtime from keystatic admin page to resolve collision with generateStaticParams

--- app/keystatic/[[...keystatic]]/page.tsx
+++ app/keystatic/[[...keystatic]]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-export const runtime = 'edge'
+// Remove Edge runtime as it conflicts with generateStaticParams
+// and Keystatic needs to run as a SPA in the browser here.
 
 export async function generateStaticParams() {
     return [{ keystatic: [] }]
